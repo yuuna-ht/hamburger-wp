6af855ce4f48ec091574fa3d9fcc8ca49bc41be0
yuuna-ht <yuna.hata29@gmail.com>
2024-07-05 00:41:20 +0900

    最終調整

--- article.php
+++ article.php
@@ -2,13 +2,14 @@
     <article class="p-article">
         <!-- articleタグ内で投稿の内容を表示 -->
         <?php the_content(); ?>
+    </article>
         <?php
         $args = array(
-            'before'      => '<div class="page-split">',
+            'before'      => '<div class="p-article-pagenation">',
             // ラッパー用の開始タグ
             'after'       => '</div>',
             // ラッパーの閉じタグ
-            'link_before' => '<span>',
+            'link_before' => '<span class="p-article-pagenation__notspList c-roboto">',
             // リンクの前に表示するタグ
             'link_after'  => '</span>',
             // リンクの後ろに表示するタグ
@@ -17,5 +18,4 @@
         // 指定されたオプションを使って、ページ分割リンクを出力。
         // 投稿が複数ページに分割されている場合にそれぞれのページへのリンクが生成され、指定されたHTMLが各要素に適用される
         ?>
-    </article>
 </div>
